7befff26e7d4b090eebfa080363a484c4d2fb314
Ryan Morton <morton.ryan92@gmail.com>
2020-08-02 14:37:27 -0700

    Probably a broken build

--- collection/__init__.py
+++
@@ -1,1 +0,0 @@
-from collection.Collection import Collection
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-    name='collection',         # How you named your package folder (MyLib)
+    name='src',         # How you named your package folder (MyLib)
     packages=['collection'],   # Chose the same as "name"
     version='0.1',      # Start with a small number and increase it with every change you make
     # Chose a license from here: https://help.github.com/articles/licensing-a-repository
